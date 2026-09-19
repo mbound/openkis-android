0accc80505423b93ddccfd66e41393071e739ff9
mbound <8779548+mbound@users.noreply.github.com>
2026-09-20 00:03:46 +0200

    Bump version to 1.3.7 for annotation release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.openkis.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "1.3.6"
+        versionCode = 29
+        versionName = "1.3.7"
     }
 
     signingConfigs {
