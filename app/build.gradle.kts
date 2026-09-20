d680199e557da13305f70c8fa24a715acc6a8804
mbound <8779548+mbound@users.noreply.github.com>
2026-09-21 00:12:54 +0200

    Bump version to 1.3.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.openkis.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 30
-        versionName = "1.3.8"
+        versionCode = 31
+        versionName = "1.3.9"
     }
 
     signingConfigs {
